b710e70e9df91f99a08bad4883bd1b1332a6eac0
Sebastian Amaya <sjamaya@uninorte.edu.co>
2020-12-07 13:22:58 -0500

    back

---
+++ back/proyecto-back/index.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import request
+app= Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hola mundo"
+
+@app.route('/params')
+def params():
+    params=request.args.get('params1','No contiene parametros')
+    return params
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8000)
