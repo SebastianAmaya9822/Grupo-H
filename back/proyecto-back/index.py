0f7f985dd2796e886e6b76d53c7fea3ced4eed45
Sebastian Amaya <sjamaya@uninorte.edu.co>
2020-12-09 09:30:53 -0500

    cors

--- back/proyecto-back/index.py
+++ back/proyecto-back/index.py
@@ -3,7 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/web'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
